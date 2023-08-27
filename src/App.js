8b5f0434b08dcc603ac72c35053b4cdc316bb00a
Rohit sharma <rs2281708@gmail.com>
2023-08-27 22:07:13 +0530

    First commit

---
+++ src/App.js
@@ -0,0 +1,219 @@
+import React from 'react'
+
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+
+const App = () => {
+  return <React.Fragment>
+   
+    <h5 className="text-center text-danger text-capitalize my-5">Learn from scientists,research scholers from the top institutes in the world</h5>
+    <div class="container text-center">
+  <div className="row row-cols-4">
+    <div className="col"><div class="card" >
+  <img src="./images/cambridge.png" class="card-img-top" alt="..." height="205px"/>
+  <div class="card-body">
+    <h5 class="card-title">Cambridge University</h5>
+    <p class="card-text">we offer undergraduate degree courses in the arts and humanities, social sciences, sciences and engineering.</p>
+    <a href="https://economictimes.indiatimes.com/news/how-to/how-to-get-admission-in-prestigious-universites-like-harvard-mit-oxford-cambridge/articleshow/102779774.cms" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div></div>
+    <div className="col"><div class="card" >
+  <img src="./images/harvard.jpg" class="card-img-top" alt="..."height="205px" />
+  <div class="card-body">
+    <h5 class="card-title">Harvard University</h5>
+    <p class="card-text">we offer undergraduate degree courses in the arts and humanities, social sciences, sciences and engineering.</p>
+    <a href="https://economictimes.indiatimes.com/news/how-to/how-to-get-admission-in-prestigious-universites-like-harvard-mit-oxford-cambridge/articleshow/102779774.cms" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div></div>
+    <div className="col"><div class="card" >
+  <img src="./images/california.png" class="card-img-top" alt="..."height="205px" />
+  <div class="card-body">
+    <h5 class="card-title">California University</h5>
+    <p class="card-text">we offer undergraduate degree courses in the arts and humanities, social sciences, sciences and engineering.</p>
+    <a href="https://economictimes.indiatimes.com/news/how-to/how-to-get-admission-in-prestigious-universites-like-harvard-mit-oxford-cambridge/articleshow/102779774.cms" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div></div>
+    <div className="col"><div class="card" >
+  <img src="./images/eth zurich.png" class="card-img-top" alt="..."height="205px" />
+  <div class="card-body">
+    <h5 class="card-title">zurich</h5>
+    <p class="card-text">we offer undergraduate degree courses in the arts and humanities, social sciences, sciences and engineering.</p>
+    <a href="https://economictimes.indiatimes.com/news/how-to/how-to-get-admission-in-prestigious-universites-like-harvard-mit-oxford-cambridge/articleshow/102779774.cms" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div></div>
+<div className="col"><div class="card" >
+  <img src="./images/max plank.jpg" class="card-img-top" alt="..."height="205px" />
+  <div class="card-body">
+    <h5 class="card-title">Max planl</h5>
+    <p class="card-text">we offer undergraduate degree courses in the arts and humanities, social sciences, sciences and engineering.</p>
+    <a href="https://economictimes.indiatimes.com/news/how-to/how-to-get-admission-in-prestigious-universites-like-harvard-mit-oxford-cambridge/articleshow/102779774.cms" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div></div>
+<div className="col"><div class="card" >
+  <img src="./images/edinberg.png" class="card-img-top" alt="..."height="205px" />
+  <div class="card-body">
+    <h5 class="card-title">Edinberg University</h5>
+    <p class="card-text">we offer undergraduate degree courses in the arts and humanities, social sciences, sciences and engineering.</p>
+    <a href="https://economictimes.indiatimes.com/news/how-to/how-to-get-admission-in-prestigious-universites-like-harvard-mit-oxford-cambridge/articleshow/102779774.cms" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div></div>
+<div className="col"><div class="card" >
+  <img src="./images/john hopkins.png" class="card-img-top" alt="..."height="205px" />
+  <div class="card-body">
+    <h5 class="card-title">John hopkins</h5>
+    <p class="card-text">we offer undergraduate degree courses in the arts and humanities, social sciences, sciences and engineering.</p>
+    <a href="https://economictimes.indiatimes.com/news/how-to/how-to-get-admission-in-prestigious-universites-like-harvard-mit-oxford-cambridge/articleshow/102779774.cms" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div></div>
+<div className="col"><div class="card" >
+  <img src="./images/michigan.png" class="card-img-top" alt="..."height="205px" />
+  <div class="card-body">
+    <h5 class="card-title">michigan state</h5>
+    <p class="card-text">we offer undergraduate degree courses in the arts and humanities, social sciences, sciences and engineering.</p>
+    <a href="https://economictimes.indiatimes.com/news/how-to/how-to-get-admission-in-prestigious-universites-like-harvard-mit-oxford-cambridge/articleshow/102779774.cms" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div></div>
+<div className="col"><div class="card" >
+  <img src="./images/illinois.png" class="card-img-top" alt="..."height="205px" />
+  <div class="card-body">
+    <h5 class="card-title">illinois University</h5>
+    <p class="card-text">we offer undergraduate degree courses in the arts and humanities, social sciences, sciences and engineering.</p>
+    <a href="https://economictimes.indiatimes.com/news/how-to/how-to-get-admission-in-prestigious-universites-like-harvard-mit-oxford-cambridge/articleshow/102779774.cms" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div></div>
+<div className="col"><div class="card" >
+  <img src="./images/humbolt.png" class="card-img-top" alt="..."height="205px" />
+  <div class="card-body">
+    <h5 class="card-title">Humbolt university</h5>
+    <p class="card-text">we offer undergraduate degree courses in the arts and humanities, social sciences, sciences and engineering.</p>
+    <a href="https://economictimes.indiatimes.com/news/how-to/how-to-get-admission-in-prestigious-universites-like-harvard-mit-oxford-cambridge/articleshow/102779774.cms" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div></div>
+<div className="col"><div class="card" >
+  <img src="./images/america.png" class="card-img-top" alt="..."height="205px" />
+  <div class="card-body">
+    <h5 class="card-title">American University</h5>
+    <p class="card-text">we offer undergraduate degree courses in the arts and humanities, social sciences, sciences and engineering.</p>
+    <a href="https://economictimes.indiatimes.com/news/how-to/how-to-get-admission-in-prestigious-universites-like-harvard-mit-oxford-cambridge/articleshow/102779774.cms" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div></div>
+<div className="col"><div class="card" >
+  <img src="./images/queen.png" class="card-img-top" alt="..."height="205px" />
+  <div class="card-body">
+    <h5 class="card-title">Queen University</h5>
+    <p class="card-text">we offer undergraduate degree courses in the arts and humanities, social sciences, sciences and engineering.</p>
+    <a href="https://economictimes.indiatimes.com/news/how-to/how-to-get-admission-in-prestigious-universites-like-harvard-mit-oxford-cambridge/articleshow/102779774.cms" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div></div>
+
+  </div>
+</div>
+<h3 className="text-center text-danger text-capitalize my-5">Meet Your Mentors</h3>
+<div className="container text-center">
+  <div className="row row-cols-4">
+    <div className="col"><div class="card" >
+  <img src="./images/mentors.jpg" class="card-img-top" alt="... "height="250px"/>
+  <div class="card-body">
+    <h5 class="card-title">Mentor</h5>
+    <p class="card-text">By being collaborative, innovative, dedicated, supportive, generous and committed, mentors provide guidance and mentorship to help ensure their mentees reach their goals.</p>
+    <a href="https://www.makeuseof.com/top-platforms-find-mentor/" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div></div>
+    <div className="col"><div class="card" >
+  <img src="./images/mentors.jpg" class="card-img-top" alt="..."height="250px" />
+  <div class="card-body">
+    <h5 class="card-title">Mentor</h5>
+    <p class="card-text">By being collaborative, innovative, dedicated, supportive, generous and committed, mentors provide guidance and mentorship to help ensure their mentees reach their goals.</p>
+    <a href="https://www.makeuseof.com/top-platforms-find-mentor/" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div></div>
+    <div className="col"><div class="card" >
+  <img src="./images/mentors.jpg" class="card-img-top" alt="..."height="250px" />
+  <div class="card-body">
+    <h5 class="card-title">Mentor</h5>
+    <p class="card-text">By being collaborative, innovative, dedicated, supportive, generous and committed, mentors provide guidance and mentorship to help ensure their mentees reach their goals.</p>
+    <a href="https://www.makeuseof.com/top-platforms-find-mentor/" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div></div>
+    <div className="col"><div class="card" >
+  <img src="./images/mentors.jpg" class="card-img-top" alt="..."height="250px" />
+  <div class="card-body">
+    <h5 class="card-title">Mentor</h5>
+    <p class="card-text">By being collaborative, innovative, dedicated, supportive, generous and committed, mentors provide guidance and mentorship to help ensure their mentees reach their goals.</p>
+    <a href="https://www.makeuseof.com/top-platforms-find-mentor/" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div></div>
+<div className="col"><div class="card" >
+  <img src="./images/mentors.jpg" class="card-img-top" alt="..."height="250px" />
+  <div class="card-body">
+    <h5 class="card-title">Mentor</h5>
+    <p class="card-text">By being collaborative, innovative, dedicated, supportive, generous and committed, mentors provide guidance and mentorship to help ensure their mentees reach their goals.</p>
+    <a href="https://www.makeuseof.com/top-platforms-find-mentor/" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div></div>
+<div className="col"><div class="card" >
+  <img src="./images/mentors.jpg" class="card-img-top" alt="..."height="250px" />
+  <div class="card-body">
+    <h5 class="card-title">Mentor</h5>
+    <p class="card-text">By being collaborative, innovative, dedicated, supportive, generous and committed, mentors provide guidance and mentorship to help ensure their mentees reach their goals.</p>
+    <a href="https://www.makeuseof.com/top-platforms-find-mentor/" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div></div>
+<div className="col"><div class="card" >
+  <img src="./images/mentors.jpg" class="card-img-top" alt="..."height="250px" />
+  <div class="card-body">
+    <h5 class="card-title">Mentor</h5>
+    <p class="card-text">By being collaborative, innovative, dedicated, supportive, generous and committed, mentors provide guidance and mentorship to help ensure their mentees reach their goals.</p>
+    <a href="https://www.makeuseof.com/top-platforms-find-mentor/" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div></div>
+<div className="col"><div class="card" >
+  <img src="./images/mentors.jpg" class="card-img-top" alt="..."height="250px" />
+  <div class="card-body">
+    <h5 class="card-title">Mentor</h5>
+    <p class="card-text">By being collaborative, innovative, dedicated, supportive, generous and committed, mentors provide guidance and mentorship to help ensure their mentees reach their goals.</p>
+    <a href="https://www.makeuseof.com/top-platforms-find-mentor/" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div></div>
+  </div>
+</div>
+<h3 className="text-center text-danger text-capitalize my-5">Mentor and Advisor</h3>
+<div className="container text-center">
+  <div className="row">
+    <div className="col">
+      <div class="card" >
+  <img src="./images/omkar.jpg" class="card-img-top" alt="..."height="300px" />
+  <div class="card-body">
+    <h5 class="card-title">Dr Omkar</h5>
+    <p class="card-text">Principal project scientist</p>
+    <a href="linkedin.com/in/dr-omkar-kushwaha-28177215" class="btn btn-primary">Msg him now</a>
+  </div>
+</div>
+    </div>
+    <div className="col">
+      <div class="card" >
+  <img src="./images/akhil.jpg" class="card-img-top" alt="..."height="300px" />
+  <div class="card-body">
+    <h5 class="card-title">Akhil Tripathi</h5>
+    <p class="card-text">Co-founder at SciAstra</p>
+    <a href="linkedin.com/in/akhiltripathisciastra" class="btn btn-primary">Msg him now</a>
+  </div>
+</div>
+    </div>
+    <div className="col">
+      <div class="card" >
+  <img src="./images/vivek.jpg" class="card-img-top" alt="..."height="300px" />
+  <div class="card-body">
+    <h5 class="card-title">Vivek Dwivedi</h5>
+    <p class="card-text">Founder & CEO - Sciastra.com</p>
+    <a href="linkedin.com/in/vivekdwivedisciastra" class="btn btn-primary">Msg him now</a>
+  </div>
+</div>
+    </div>
+  </div>
+</div>
+
+  </React.Fragment>
+  
+}
+
+export default App
---
+++ src/card.jsx
@@ -0,0 +1,13 @@
+// MyComponent.jsx
+
+import React from 'react';
+
+function card() {
+    return (
+        <div>
+            <h1>Hello from card!</h1>
+        </div>
+    );
+}
+
+export default card;
